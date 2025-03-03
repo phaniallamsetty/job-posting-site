2f5faf807f3b0a2380f4ed10346edf68ae6a73e6
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2025-03-03 13:48:32 -0500

    Custom 404 page, active nav link and jobs page content

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,8 @@
 import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
-const JobListings = () => {
-    const recentJobs = jobs.slice(0, 3)
+const JobListings = ({ isHome = false }) => {
+    let jobListings = isHome ? jobs.slice(0, 3) : jobs
     
     return (
         <section className="bg-blue050 px-4 py-10">
@@ -11,7 +11,7 @@ const JobListings = () => {
                     Browse Jobs
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentJobs.map((job) => (                        
+                    {jobListings.map((job) => (                        
                         <JobListing key={job.id} job={job} />
                     ))}
                 </div>
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,6 +1,10 @@
+import JobListings from '../components/JobListings'
+
 const JobsPage = () => {
     return (
-        <h1>JobsPage</h1>
+        <section className="bg-blue-50 px-4 py-6">
+            <JobListings />
+        </section>
     )
 }
 
