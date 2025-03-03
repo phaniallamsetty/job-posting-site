9d125fd1b46f356325563bb89a621c3e11c4cb9c
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2025-03-03 14:24:51 -0500

    Added spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,20 +1,46 @@
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner'
 
 const JobListings = ({ isHome = false }) => {
-    let jobListings = isHome ? jobs.slice(0, 3) : jobs
-    
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            const apiUrl = isHome ? 'http://localhost:3000/jobs?_limit=3' : 'http://localhost:3000/jobs';
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.log("error fetching data", error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchJobs();
+    }, []);
+
     return (
         <section className="bg-blue050 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                    {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListings.map((job) => (                        
-                        <JobListing key={job.id} job={job} />
-                    ))}
-                </div>
+                
+                    { loading ? 
+                        (<Spinner loading={loading} />) : 
+                        (
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                {jobs.map((job) => (
+                                    <JobListing key={job.id} job={job} />
+                                ))}
+                            </div>
+                        )
+                    }
+                
             </div>
         </section>
     )
